c44be2e1295afe7d1e7d3c2b8a344ae2118a4ca5
StepashkaSuicide <churbakov.evgen@mail.ru>
2022-06-07 17:51:18 +0300

    home work 4 useState toggle

---
+++ src/Accordion/UncontrolledAccordion.tsx
@@ -0,0 +1,47 @@
+import React, {useState} from "react";
+
+
+type UncontrolledAccordionPropsType = {
+    titleValue: string
+    // collapsed: boolean
+}
+
+function UncontrolledAccordion(props: UncontrolledAccordionPropsType) {
+
+
+    const [collapsed, setCollapsed] = useState(false)
+
+    const handler = () => {
+        if (collapsed) {
+            setCollapsed(false)
+        } else {
+            return setCollapsed(true)
+        }
+    }
+
+    return (
+        <div>
+            <AccordionTitle title={props.titleValue}/>
+            <button onClick={handler}>toggle</button>
+            {!collapsed && <AccordionBody/>}
+        </div>
+    )
+}
+
+type AccordionTitlePropsType = {
+    title: string
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    return <h3>{props.title}</h3>
+}
+
+function AccordionBody() {
+    return <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+}
+
+export default UncontrolledAccordion
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import OnOff from "./OnOff/OnOff";
+import UncontrolledAccordion from "./Accordion/UncontrolledAccordion";
 
 
 function App() {
-  return (
-    <div className="App">
-   565
-    </div>
-  );
+    return (
+        <div>
+            <UncontrolledAccordion titleValue={'Menu'} />
+            <UncontrolledAccordion titleValue={'Users'}/>
+            Article 2
+            {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
+
+            <OnOff/>
+        </div>
+    );
 }
+
+type PageTitlePropsType = {
+    title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+    return <h1>{props.title}</h1>
+}
+
 
 export default App;
