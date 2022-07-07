6d1bf48edd819f593ddc2d5477122b11cd899908
StepashkaSuicide <churbakov.evgen@mail.ru>
2022-07-07 17:02:09 +0300

    controlled input, checked, select + storybook

---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+
+export default {
+    title: "ReactMemo"
+}
+
+const NewMessagesCounter = (props: {count: number})=> {
+
+    return <div>{props.count}</div>
+}
+
+const UsersSecret = (props: {users: Array<string>})=> {
+
+    return <div>{props.users.map((u, i)=> <div key={i}>{u}</div>)}</div>
+}
+
+const Users = React.memo(UsersSecret)
+
+
+export const Example1 = ()=> {
+
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(['Dimych', 'Valera', 'Artem'])
+
+    const addUser = ()=> {
+        let newUser = [...users, 'Sveta' + new Date().getTime()]
+        setUsers(newUser)
+    }
+
+
+return <>
+        <button onClick={()=> setCounter((counter+ 1))}>+</button>
+        <button onClick={addUser}>addUser</button>
+    <NewMessagesCounter count={counter}/>
+    <Users users={users}/>
+    </>
+}
